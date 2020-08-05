76297e3fa528182be112ff6b5be3fe39c0f7b9ae
Clemens Hemmerling <clemens@getkor.io>
2020-08-05 16:40:44 -0600

    Create endpoints for update client and show single client

--- db/migrate/20200805220204_add_fields_to_client.rb
+++ db/migrate/20200805220204_add_fields_to_client.rb
@@ -1,5 +1,5 @@
 class AddFieldsToClient < ActiveRecord::Migration[6.0]
   def change
-    add_column :clients, :count, :int, auto_increment: true
+    add_column :clients, :count, :int
   end
 end
--- db/migrate/20200805220743_add_fields_to_clients.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldsToClients < ActiveRecord::Migration[6.0]
-  def change
-    add_column :clients, :count, :integer
-  end
-end
